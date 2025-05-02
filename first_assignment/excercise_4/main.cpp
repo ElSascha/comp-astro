316af6f68e789dc7fdb10a68b805910a6a9fcfaf
El_Sascha <sascha.eckstein@uni-tuebingen.de>
2025-05-02 08:48:49 +0200

    wokring on excercise 4

---
+++ first_assignment/excercise_4/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <limits.h>
+
+double find_pi_recursion(int n){
+    return find_pi_recursion_worker(n,sqrt(2.0));
+    
+}
+
+double find_pi_recursion_worker(int n, double A_n){
+    double A_npo;
+    if (n == 0){
+        return 0.0;
+    }
+    else{
+        A_npo = std::exp
+    }
+    
+}
+
+int main(){
+   
+
+}
