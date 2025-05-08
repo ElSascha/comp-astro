b1320078d834634c30c0cad66ef7b61446addd9a
Jannis Hollmann <janni.hollmann@gmail.com>
2025-05-08 11:17:11 +0200

    Relative path function added to python file

--- first_assignment/excercise_4/script.py
+++ first_assignment/excercise_4/script.py
@@ -1,8 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-pi_recursion = np.loadtxt(r'/home/sasch/Studium/comp-astro/first_assignment/excercise_4/pi_recursion_data.txt',delimiter=';')
-pi_kahan = np.loadtxt(r'/home/sasch/Studium/comp-astro/first_assignment/excercise_4/pi_Kahan_data.txt',delimiter=';')
+def rel_path(filename):
+    """
+    Given a filename, returns the absolute path to the file relative to the caller's location.
+
+    Args:
+        filename (str): The name of the file.
+
+    Returns:
+        str: The absolute path to the file relative to the caller's location.
+    """
+    caller_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(caller_dir, filename)
+
+pi_recursion = np.loadtxt(rel_path('pi_recursion_data.txt'),delimiter=';')
+pi_kahan = np.loadtxt(rel_path('pi_Kahan_data.txt'),delimiter=';')
 
 plt.figure(figsize=(10, 5))
 plt.scatter(pi_recursion[:,0],abs( pi_recursion[:,1] - np.pi), label='Pi recursion', color='blue',marker='x')
