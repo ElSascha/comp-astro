34d1267faeea254ec068576e7ad0b55121318d2d
El_Sascha <sascha.eckstein@uni-tuebingen.de>
2025-05-12 12:56:02 +0200

    added documentation (totally wrote it myself)

--- second_assignment/exercise_3/main.cpp
+++ second_assignment/exercise_3/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+/*
+
+*/
+/**
+ * @brief Solves a quadratic equation of the form ax^2 + bx + c = 0.
+ *
+ * @param param Pointer to an array of three doubles representing the coefficients [a, b, c].
+ * @return Pointer to an array of doubles containing the real roots of the equation:
+ *         - If the equation is linear (a == 0, b != 0), returns a pointer to an array of size 1 with the single root.
+ *         - If the equation has two real roots, returns a pointer to an array of size 2 with the roots.
+ *         - If there are no real roots, returns nullptr.
+ * 
+ * @note The returned array is dynamically allocated and must be deleted by the caller to avoid memory leaks.
+ */
 
 double* solve_quadratic(double* param){
     //Linear case
