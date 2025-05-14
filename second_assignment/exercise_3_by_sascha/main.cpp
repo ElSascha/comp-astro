ab081e71ad49c77118006f5f763df90c2e459377
El_Sascha <sascha.eckstein@uni-tuebingen.de>
2025-05-14 15:02:39 +0200

    alles fertig

--- second_assignment/exercise_3_by_sascha/main.cpp
+++ second_assignment/exercise_3_by_sascha/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <limits.h>
 /*
 
 */
@@ -23,26 +24,40 @@ double* solve_quadratic(double* param){
         res[0] = -param[2] / param[1];
         return res;
     }
-    
-    if (param[1] * param[1] - 4 * param[0] * param[2] < param[1] * param[1]){
-        double* res = new double[2];
-        res[1] = (-param[1] - sqrt(param[1]*param[1] - 4*param[0]*param[2])) / (2*param[0]);
-        res[0] = (-param[1] + sqrt(param[1]*param[1] - 4*param[0]*param[2])) / (2*param[0]);
+    double a = param[0];
+    double b = param[1];
+    double c = param[2];
+    double* res = new double[2];
+    if(b*b - 4.0*a*c < b*b){
+        res[1] = (-b - sqrt(b*b - 4.0*a*c)) / (2.0*a);
+        res[0] = (-b + sqrt(b*b - 4.0*a*c)) / (2.0*a);
         return res;
     }
     else{
-        return nullptr;
+        res[0] = -(2.0*c/(b + sqrt(b*b - 4.0*a*c)));
+        res[1] = (-b - sqrt(b*b - 4.0*a*c)) / (2.0*a);
+        return res;
     }
 }
 
 
 int main(){
 
-    double param[] = {1, 1, 1e-6}; // Coefficients of the quadratic equation ax^2 + bx + c = 0
-    double* result = solve_quadratic(param);
-    std::cout << "x_1: " << result[0] << ", x_2: " << result[1] << std::endl;
-    // Free the allocated memory
-    delete[] result;
+    double* param = new double[3];
+    double* result;
+    param[0] = 1; 
+    param[1]= 1;
+    std::cout.precision(20);
+    std::cout<<"a = 1, b = 1, c = 1e-x"<<std::endl;
+    for (int i = 1 ; i< 30; i++){
+        std::cout << "c = " << std::pow(10.0, -i) << std::endl;
+        param[2] = std::pow(10.0, -i);
+        result = solve_quadratic(param);
+        std::cout << "x_1: " << result[0] << ", x_2: " << result[1] << std::endl;
+        // Free the allocated memory
+        delete[] result;
+
+    }
     return 0;
 
 }
