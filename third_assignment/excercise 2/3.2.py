4e9aa9ee8e58583e2113b21e3ff91083b51c8012
Jannis Hollmann <janni.hollmann@gmail.com>
2025-05-26 15:47:35 +0200

    started 3.2 in pyhton

---
+++ third_assignment/excercise 2/3.2.py
@@ -0,0 +1,6 @@
+import scipy.integrate
+
+M = 5.974*10**24 # kg; Mass of the Earth
+m = 7.348*10**22 # kg; Mass of the Moon
+R = 3.844*10**8  # m; Distance between Earth and Moon
+omega = 2.662*10**(-6) # 1/s; Orbital frequency of Moon and satellite
